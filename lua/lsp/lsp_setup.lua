3179932c77a43c7b47e3be0516772cb50232a2e9
kyon45 <2220368487@qq.com>
2023-09-23 10:56:06 +0800

    feat(lsp): config `rust-tools`

--- lua/lsp/lsp_setup.lua
+++ lua/lsp/lsp_setup.lua
@@ -12,7 +12,7 @@ local servers = {
   'jsonls',
   -- 'eslint',
   -- Other
-  'rust_analyzer',
+  'rust_analyzer', -- Cargo conventions: https://doc.rust-lang.org/cargo/guide/project-layout.html
 }
 
 require('mason').setup()
@@ -50,12 +50,15 @@ rt.setup({
   -- these override the defaults set by rust-tools.nvim
   -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
   server = {
-    on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-    end,
+    capabilities = require('lsp.lsp_handlers').capabilities,
+    on_attach = require('lsp.lsp_handlers').on_attach,
+    -- on_attach = function(_, bufnr)
+    --   -- Hover actions
+    --   vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+    --   -- Code action groups
+    --   vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+    -- end,
+
     -- standalone file support
     -- setting it to false may improve startup time
     -- standalone = true,
