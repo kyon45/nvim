303f3742fd1ef586438381c7afa95860b32044c3
kyon45 <2220368487@qq.com>
2023-11-03 14:30:08 +0800

    feat(lsp): config diagnostic signs

--- lua/lsp/lsp_handlers.lua
+++ lua/lsp/lsp_handlers.lua
@@ -9,10 +9,22 @@ Module.capabilities = cmp_nvim_lsp.default_capabilities(Module.capabilities)
 
 -- setup
 Module.setup = function()
-  -- [TODO] diagnostic settings
+  -- diagnostic settings
+  local signs = {
+		{ name = "DiagnosticSignError", text = "" },
+		{ name = "DiagnosticSignWarn", text = "" },
+		{ name = "DiagnosticSignHint", text = "" },
+		{ name = "DiagnosticSignInfo", text = "" },
+	}
+  for _, sign in ipairs(signs) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+  end
+
   local config = {
     virtual_text = false, -- disable inline virtual text
-    -- signs = {},
+    signs = {
+      active = signs, -- show signs
+    },
     update_in_insert = true,
     underline = true,
     severity_sort = true,
