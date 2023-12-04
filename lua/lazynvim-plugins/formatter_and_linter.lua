9f66f08a529ff71e290c1b9e0fe6894bc065a93a
kyon45 <2220368487@qq.com>
2023-12-04 10:43:58 +0800

    feat(linter): add `nvim-lint`

--- lua/lazynvim-plugins/formatter_and_linter.lua
+++ lua/lazynvim-plugins/formatter_and_linter.lua
@@ -40,4 +40,31 @@ return {
       end, { desc = "Format file or range (in visual mode)" })
     end,
   },
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      local lint = require("lint")
+
+      local eslint_d = { "eslint_d" }
+      lint.linters_by_ft = {
+        javascript = eslint_d,
+        typescript = eslint_d,
+        javascriptreact = eslint_d,
+        typescriptreact = eslint_d,
+      }
+
+      local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+      vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+        group = lint_augroup,
+        callback = function()
+          lint.try_lint()
+        end,
+      })
+
+      vim.keymap.set("n", "<leader>l", function()
+        lint.try_lint()
+      end, { desc = "Trigger linting for current file" })
+    end,
+  },
 }
