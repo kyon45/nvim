b7b2359572cd8ccc7b25b22fe7b74e9352b06321
kyon45 <2220368487@qq.com>
2024-03-28 22:14:07 +0800

    feat(telescope): add `live_grep_raw` command

--- lua/lazynvim-plugins/telescope.lua
+++ lua/lazynvim-plugins/telescope.lua
@@ -10,12 +10,23 @@ return {
         noremap = true,
         silent = true,
       }
-      -- local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>', opt)
       vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', opt)
       vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', opt)
       vim.keymap.set('n', '<leader>fh', '<cmd>Telescope buffers<CR>', opt)
       vim.keymap.set('n', '<leader>fk', '<cmd>Telescope keymaps<CR>', opt)
+
+      local builtin = require('telescope.builtin')
+      local live_grep_raw = function ()
+        builtin.live_grep({
+          additional_args = {
+            -- https://github.com/BurntSushi/ripgrep/blob/master/GUIDE.md#common-options
+            '--fixed-strings',
+          },
+          prompt_title = 'Live Grep (Raw)',
+        })
+      end
+      vim.keymap.set('n', '<leader>fgr', live_grep_raw, opt)
     end,
   },
 }
