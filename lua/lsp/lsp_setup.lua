51cc7ee99f217710db76240b6c8381ce1d019a69
kyon45 <2220368487@qq.com>
2024-12-06 00:12:53 +0800

    fix: patch

--- lua/lsp/lsp_setup.lua
+++ lua/lsp/lsp_setup.lua
@@ -8,7 +8,8 @@ local servers = {
   -- Web Dev
   'html',
   'cssls',
-  'tsserver', -- required by `typescript-tools.nvim`
+  -- 'tsserver', -- required by `typescript-tools.nvim`
+  'ts_ls',
   'jsonls',
   -- Other
   'rust_analyzer', -- Cargo conventions: https://doc.rust-lang.org/cargo/guide/project-layout.html
