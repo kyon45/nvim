563e4d09cd1de97885a7fef23ff4b5e1a851c7dc
kyon45 <2220368487@qq.com>
2024-12-06 19:38:56 +0800

    feat: -- nvim-tree & buffer keymaps

--- lua/lazynvim-plugins/nvim-tree.lua
+++ lua/lazynvim-plugins/nvim-tree.lua
@@ -2,10 +2,17 @@ return {
   {
     'nvim-tree/nvim-tree.lua',
     version = '*',
-    dependencies = {'nvim-tree/nvim-web-devicons'},
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
       require('nvim-tree').setup()
-    end
-  }
-}
 
+      local opt = {
+        noremap = true,
+        silent = true,
+      }
+      vim.keymap.set('n', '<leader>bb', '<cmd>NvimTreeToggle<CR>', opt)
+      vim.keymap.set('n', '<leader>bc', '<cmd>BufferClose<CR>', opt)
+      vim.keymap.set('n', '<leader>bt', '<cmd>BufferCloseAllButCurrent<CR>', opt)
+    end,
+  },
+}
