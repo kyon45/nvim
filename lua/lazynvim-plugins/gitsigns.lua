04c19cb3d3ca1f03a0275d2865e14a3117b8821a
kyon45 <2220368487@qq.com>
2025-01-17 11:22:01 +0800

    feat(gitsigns): -- add keymaps

--- lua/lazynvim-plugins/gitsigns.lua
+++ lua/lazynvim-plugins/gitsigns.lua
@@ -1,4 +1,5 @@
 return {
+  ---@type LazySpec
   {
     'lewis6991/gitsigns.nvim',
     config = function()
@@ -9,8 +10,37 @@ return {
           delay = 200,
           ignore_whitespace = false,
         },
-      })
-    end
-  }
-}
+        on_attach = function(bufnr)
+          local gitsigns = require('gitsigns')
 
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map('n', ']c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({ ']c', bang = true })
+            else
+              gitsigns.nav_hunk('next')
+            end
+          end)
+
+          map('n', '[c', function()
+            if vim.wo.diff then
+              vim.cmd.normal({ '[c', bang = true })
+            else
+              gitsigns.nav_hunk('prev')
+            end
+          end)
+
+          -- Actions
+          --- hunks
+          map('n', '<leader>hp', gitsigns.preview_hunk)
+        end,
+      })
+    end,
+  },
+}
